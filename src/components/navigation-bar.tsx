61856189a2796a1e48f109d139ff79691028d9c8
Rich Cahill <rich@rc3.me>
2024-10-26 20:55:48 +0100

    tweaking the intial navigation bar

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -11,18 +11,25 @@ const NavigationBar: React.FC = () => {
   };
 
   return (
-    <nav className="flex justify-between items-center p-4 max-w-3xl mx-auto">
-      <div className="logo">
-        <Link href="/">
-          <Logo className="text-gray-300 h-8 w-8 hover:scale-105 hover:text-gray-900 transition-all dark:text-gray-50" />
-        </Link>
-      </div>
-      <div className="">
-        <Button onClick={handleSignOut} variant="ghost">
-          Sign Out
-        </Button>
-      </div>
-    </nav>
+    <div className="max-w-3xl mx-auto p-2">
+      <nav className="flex justify-between items-center pl-4 p-2 w-full bg-gray-100 rounded-full">
+        <div className="logo">
+          <Link href="/">
+            <Logo className="text-gray-400 h-7 w-7 hover:scale-105 hover:text-gray-900 transition-all dark:text-gray-50" />
+          </Link>
+        </div>
+        <div className="">
+          {/* ignoring the standard shadcn button variants here just for sign out */}
+          <Button
+            onClick={handleSignOut}
+            className="rounded-full bg-white text-gray-900 shadow-none hover:bg-gray-200 hover:text-gray-950 transition-all"
+            size="sm"
+          >
+            Sign Out
+          </Button>
+        </div>
+      </nav>
+    </div>
   );
 };
 
