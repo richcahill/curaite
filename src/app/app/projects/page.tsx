485501802a8b4ddaac26c894841ff3ea5ef563b9
Rich Cahill <rich@rc3.me>
2024-10-29 12:10:29 +0000

    tiny bit of pruning on the projects placeholder page

--- src/app/app/projects/page.tsx
+++ src/app/app/projects/page.tsx
@@ -3,13 +3,11 @@
 export default function Projects() {
   return (
     <div className="flex h-full flex-col items-center justify-start p-4 max-w-3xl mx-auto pb-20 gap-4 ">
-      <div className="text-sm flex flex-col flex-grow items-start justify-start gap-6 bg-gray-50 rounded-lg p-4 pb-8 w-full">
+      <div className="text-sm flex-grow space-y-2 bg-gray-50 rounded-lg p-4 pb-8 w-full">
         <p className="max-w-prose">
           Collections of assets grouped by project with AI generated combined
           metadata.
-          <br />
         </p>
-
         <p className="text-gray-500 max-w-prose">Coming soon...</p>
       </div>
     </div>
