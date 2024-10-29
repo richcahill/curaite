1d01b52de5ab6dec13973616ba70399f74bf0015
Rich Cahill <rich@rc3.me>
2024-10-29 22:13:49 +0000

    update the design of the folio card

--- src/components/folio-card.tsx
+++ src/components/folio-card.tsx
@@ -7,14 +7,14 @@ const FolioCard: React.FC<{ folio: Folio }> = ({ folio }) => {
 
   return (
     <Link href={`/app/folios/${folio.uuid}`}>
-      <div className="aspect-square w-full border border-gray-200 bg-gray-50 rounded-sm p-3 hover:bg-white transition-colors duration-200 space-y-4">
+      <div className="aspect-video w-full border border-gray-200 bg-gray-50 rounded-sm p-3 hover:bg-white transition-colors duration-200 space-y-4">
         <h2 className="text-md text-gray-900">{folio.title}</h2>
 
         <div className="flex flex-row gap-1 flex-wrap -ml-1">
           {folio.tags?.map((tag) => (
             <div
               key={tag}
-              className="text-xs text-gray-500 bg-gray-200 rounded-full px-2 py-0.5 lowercase"
+              className="text-xs text-gray-800 font-light tracking-wide bg-gray-100 rounded-full px-2 py-0.5 lowercase"
             >
               {tag}
             </div>
