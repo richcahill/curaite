3ce0e2fba9b721ce9bcaea8c92661e3cc54972cf
Rich Cahill <rich@rc3.me>
2024-10-29 12:13:50 +0000

    placeholder index page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex h-full flex-col items-center justify-center p-8 pb-20 gap-16 sm:p-20"></div>
+    <div className="flex h-full flex-col max-w-3xl w-full mx-auto items-center justify-center p-8 pb-20 gap-16 sm:p-20">
+      <div className="text-sm flex items-center justify-center flex-grow space-y-2 text-center text-gray-400 rounded-lg p-8 w-full border border-gray-200 border-dashed">
+        Some kind of dashboard / home with things to do / tasks to try etc
+      </div>
+    </div>
   );
 }
