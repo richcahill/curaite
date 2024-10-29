314b84241572d83a2e34342a4bcaa7e2d8a61f96
Rich Cahill <rich@rc3.me>
2024-10-29 13:56:38 +0000

    showcase assets on folio page

--- src/app/app/folios/[folio]/folio-detail.tsx
+++ src/app/app/folios/[folio]/folio-detail.tsx
@@ -4,14 +4,21 @@ import {
   Breadcrumb,
   BreadcrumbList,
   BreadcrumbItem,
-  BreadcrumbLink,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { firestore, auth } from "@/lib/firebase";
-import { getDoc } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { type Folio } from "@/lib/types";
-import { doc } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+enum AssetType {
+  Image = "Image",
+  Video = "Video",
+  Document = "Document",
+}
 
 export default function FolioDetail({ folioUuid }: { folioUuid: string }) {
   const user = auth.currentUser;
@@ -19,35 +26,74 @@ export default function FolioDetail({ folioUuid }: { folioUuid: string }) {
 
   useEffect(() => {
     const fetchFolio = async () => {
-      const folioDoc = await getDoc(
-        doc(firestore, `folios/${user?.uid}/folios/${folioUuid}`)
-      );
-      const folioData = folioDoc.data() as Folio;
-      setFolio(folioData);
+      try {
+        const foliosRef = collection(firestore, `folios/${user?.uid}/folios`);
+        const q = query(foliosRef, where("uuid", "==", folioUuid));
+        const querySnapshot = await getDocs(q);
+
+        if (!querySnapshot.empty) {
+          const folioData = querySnapshot.docs[0].data() as Folio;
+          setFolio(folioData);
+          console.log("Fetched folio data:", folioData);
+        } else {
+          console.warn("No folio found with the given UUID.");
+        }
+      } catch (error) {
+        console.error("Error fetching folio:", error);
+      }
     };
     fetchFolio();
   }, [user?.uid, folioUuid]);
 
   return (
     <div className="flex h-full flex-col items-center justify-start p-4 max-w-3xl mx-auto pb-20 gap-4 ">
-      <div className="flex flex-row gap-4 justify-between w-full">
+      <div className="flex flex-row gap-4 justify-between w-full items-center">
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem>
-              <BreadcrumbLink href="/app/folios">Folios</BreadcrumbLink>
+              <Link href="/app/folios">Folios</Link>
               <BreadcrumbSeparator />
             </BreadcrumbItem>
-            <BreadcrumbItem>
-              <BreadcrumbLink href={`/app/folios/${folio?.uuid}`}>
-                {folio?.title}
-              </BreadcrumbLink>
-            </BreadcrumbItem>
+            <BreadcrumbItem>{folio?.title}</BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
+        <div className="flex flex-row gap-2">
+          <Button size="sm" variant="outline">
+            Edit
+          </Button>
+          <Button size="sm">Share</Button>
+        </div>
       </div>
 
-      <div className="flex flex-col flex-grow items-center justify-center gap-6 bg-gray-50 rounded-lg p-4 w-full">
-        hello world
+      <div className="flex flex-col flex-grow items-start justify-center gap-2 bg-gray-50 rounded-lg p-4 w-full">
+        <h2 className="text-2xl">{folio?.title}</h2>
+        <div className="text-sm text-gray-500">{folio?.description}</div>
+        <div className="flex flex-row gap-2">
+          {folio?.tags?.map((tag) => (
+            <div key={tag}>{tag}</div>
+          ))}
+        </div>
+        <div className="grid grid-cols-2 gap-2">
+          {folio?.assets?.map((item) => (
+            <div key={item.id}>
+              {item.type === AssetType.Image.toString() ? (
+                <Image
+                  src={item.storageUrl || ""}
+                  alt={""}
+                  width={300}
+                  height={200}
+                />
+              ) : (
+                <video
+                  src={item.storageUrl || ""}
+                  controls
+                  title={""}
+                  className="w-full"
+                />
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,13 +10,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-violet-800 text-violet-50 shadow hover:bg-violet-900/80 dark:bg-violet-50 dark:text-violet-900 dark:hover:bg-violet-50/90",
+          "bg-violet-800 text-violet-50 hover:bg-violet-900/80 dark:bg-violet-50 dark:text-violet-900 dark:hover:bg-violet-50/90",
         destructive:
-          "bg-red-500 text-gray-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-gray-50 dark:hover:bg-red-900/90",
+          "bg-red-500 text-gray-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-gray-50 dark:hover:bg-red-900/90",
         outline:
-          "border border-gray-200 bg-white shadow-sm hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50",
+          "border border-gray-200 bg-white hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50",
         secondary:
-          "bg-gray-100 text-gray-900 shadow-sm hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80",
+          "bg-gray-100 text-gray-900 hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80",
         ghost:
           "hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50",
         link: "text-gray-900 underline-offset-4 hover:underline dark:text-gray-50",
